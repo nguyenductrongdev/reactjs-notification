fb2ca8a86fc621f6976b86d987772af16ae4769b
ndtrong <nguyenductrong12a1@gmail.com>
2024-05-12 23:17:22 +0700

    Stable

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+// Firebase Messaging Service Worker
+
+self.addEventListener("push", (event) => {
+    const notif = event.data.json().data;
+    console.log(`${notif.title}: ${notif.body}`);
+    // event.waitUntil(self.registration.showNotification(notif.title, {
+    //     body: notif.body,
+    //     icon: notif.image,
+    //     data: {
+    //         url: notif.click_action
+    //     }
+    // }));
+
+});
+
+self.addEventListener("notificationclick", (event) => {
+
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+
+});
--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ72GZvfYH1Mz0WHImkKA6pdZvHxCiZ3I",
+  authDomain: "springbootnotifapplication.firebaseapp.com",
+  projectId: "springbootnotifapplication",
+  storageBucket: "springbootnotifapplication.appspot.com",
+  messagingSenderId: "914312176526",
+  appId: "1:914312176526:web:26026bbe9b07c7522e3adf",
+  measurementId: "G-910B39C8MZ"
+};
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
 
 function App() {
+  navigator.serviceWorker.register("/sw.js").then(registration => {
+    getToken(messaging, {
+      serviceWorkerRegistration: registration,
+      vapidKey: 'BKI6Qrxv4br-LHjMG5M2fxksmjBAPfyafkiNWJZ2gl_l66QW-jK8QX7TFnfPBvpSuQp1nyDlTyaSxUk8hagRiQ4'
+    }).then((currentToken) => {
+      if (currentToken) {
+        console.log("Token is: " + currentToken);
+        // Send the token to your server and update the UI if necessary
+        // ...
+      } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+      }
+    }).catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+      // ...
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
