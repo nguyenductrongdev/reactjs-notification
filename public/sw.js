79351bd357aa42ae51aa1924861c2e3403704b10
ndtrong <nguyenductrong12a1@gmail.com>
2025-09-03 23:38:35 +0700

    refactor: push notification

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,13 @@
 
 self.addEventListener("push", (event) => {
     const notif = event.data.json().data;
-    console.log(`${notif.title}: ${notif.body}`);
-    const channel = new BroadcastChannel('notifications');
-    channel.postMessage(notif);
+
+    const {title, body} = notif;
+    console.log(notif);
+
+    self.registration.showNotification(title, {
+        body: body,
+    }).catch((error) => {
+        console.error(error);
+    });
 });
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js";
+import {useState} from "react";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBsTHR5C1xwo6ADx3g3hnItyvv8pVbi7A8",
@@ -17,47 +17,34 @@ const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
 function App() {
-  navigator.serviceWorker.register("/sw.js").then(registration => {
-    getToken(messaging, {
-      serviceWorkerRegistration: registration,
-      vapidKey: 'BKI6Qrxv4br-LHjMG5M2fxksmjBAPfyafkiNWJZ2gl_l66QW-jK8QX7TFnfPBvpSuQp1nyDlTyaSxUk8hagRiQ4'
-    }).then((currentToken) => {
-      if (currentToken) {
-        console.log("Token is: " + currentToken);
-        // Send the token to your server and update the UI if necessary
-        // ...
-      } else {
-        // Show permission request UI
-        console.log('No registration token available. Request permission to generate one.');
-        // ...
-      }
-    }).catch((err) => {
-      console.log('An error occurred while retrieving token. ', err);
-      // ...
-    });
+  const [fcmToken, setFcmToken] = useState("");
 
-    const channel = new BroadcastChannel("notifications");
-    channel.addEventListener("message", (event) => {
-      console.log("Receive background: ", event.data);
+  if(!fcmToken){
+    navigator.serviceWorker.register("/sw.js").then(registration => {
+      getToken(messaging, {
+        serviceWorkerRegistration: registration,
+        vapidKey: 'BEi_Qpx8OA0cDxQ9DjtIW2OF8nHxwhdBU79hkiDjg5mKJQm4eK42OhsZWyWo5wYtIqDNRlnLmDRRaW7ZTHMqfEs'
+      }).then((currentToken) => {
+        if (currentToken) {
+          setFcmToken(currentToken);
+          console.log("Token is: " + currentToken);
+          // Send the token to your server and update the UI if necessary
+          // ...
+        } else {
+          // Show permission request UI
+          console.log('No registration token available. Request permission to generate one.');
+          // ...
+        }
+      }).catch((err) => {
+        console.log('An error occurred while retrieving token. ', err);
+      });
     });
-  });
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <b>FCM Token:</b>
+      <code>{fcmToken}</code>
     </div>
   );
 }
